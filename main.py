5e693b9d3a403c2304786ee00b3b081332692392
alexandrumosgoreanu <amosgoreanu@gmail.com>
2024-02-28 14:51:48 +0200

    Added regex for contact page and social media links

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
-from lxml import etree
 import re
 
 # Read csv file with the domain names and return the array containg the URL as strings
@@ -43,24 +42,27 @@ address_regex = re.compile(r'''(
                            [\s]?
                            [a-zA-Z0-9.\-\,\#]+
                            [a-zA-Z0-9\s.\-\,\#]*
-                           [0-9]{5})|(\b(PO|Post)+
-                           [a-zA-Z0-9\s.\-\,\#]*
                            [0-9]{5})''', re.VERBOSE)
+pobox_regex = re.compile(r'''(\b(PO|Post)+[a-zA-Z\s0-9.\-\,\#]*[0-9]{5})''')
+                         
 # email_regex = re.compile(r'''(
 #                         [a-zA-Z0-9._%+-]+
 #                         @
 #                         [a-zA-Z0-9.-]+
 #                         (\.[a-zA-Z]{2,4}))''', re.VERBOSE)
 
+social_media_regex = re.compile(r'^(http[s])?(://)?(www.)?(facebook|instagram|twitter|tiktok|linkedin|youtube)+(.com[/in])+[/a-zA-Z0-9.\-\,\#]*$')
 
-url = "http://" + domains_list[19]
+
+url = "http://" + domains_list[3]
 print(url)
 page = requests.get(url)
 page_html = str(page.content) 
 
-def search_phone(soup):
+def extract_phone(soup):
     matches = set()
     for groups in phone_regex.findall(soup):
+        print(groups)
         phone_numbers = '-'.join([groups[1], groups[3], groups[5]])
         if groups[8] != '':
                 phone_numbers += ' x' + groups[8]            
@@ -68,35 +70,52 @@ def search_phone(soup):
 
     return matches
 
-def search_address(soup):
+def extract_address(soup):
     matches = set()
     for groups in address_regex.findall(soup):
         matches.add("".join(groups[0]))
-        print(groups)
+    if len(matches) == 0:
+        for groups in pobox_regex.findall(soup):
+            matches.add("".join(groups[0]))
     return matches
 
-# def search_email(soup):
+def extract_links(links, url):
+    matches = set()
+    contact_page_matches = set()
+    contact_page_regex = re.compile(fr'(http[s]://)+(www.)?({url[7:]}/)+(contact|about)+[a-zA-Z0-9.\-\,\#]*')
+
+    for link in links:
+        if social_media_regex.match(link):
+            matches.add(link)
+        elif contact_page_regex.match(link):
+            contact_page_matches.add(link)
+    return matches, contact_page_matches
+
+# def extract_email(soup):
 #     matches = set()
 #     for groups in email_regex.findall(soup):
 #         matches.add("".join(groups[0]))
 #     return matches
 
 
-soup = BeautifulSoup(page.content, "lxml")
+soup = BeautifulSoup(page.content, "lxml")          # parsing with lxml for speed vs. html.parser
+links = [link['href'] for link in soup.find_all('a', href = True)]           # getting all the links
+soup_text = soup.get_text(" ", strip=True)           # converting to text in order to match phone and address
 
-
-
-soup = soup.get_text(" ", strip=True)
 with open('soup.txt', 'w', encoding="utf-8") as f:
-    f.write(soup)
-# phone = search_phone(soup)
-address = search_address('''PO Box 971201 Boca Raton FL 33497 Top salon - Hair Salon in State College Top salon  View Menu  Call now  Get directions Updates Testimonials Gallery Contact Top salon Hair Salon in State College Opening at 10:00 AM View Menu Call  (814) 954-5812 Get directions WhatsApp  (814) 954-5812 Message  (814) 954-5812 Contact Us Get Quote Find Table Make Appointment Place Order Updates Posted on Feb 20, 2024 Testimonials 6 months ago  Flag as inappropriate I am so happy with my new style!
-It was my first time getting a perm in the States. I was not sure because recently I got a bad haircut from a salon in the Magnificent Mile!
-The stylist here cut off blunt end of my hair for free and he was super meticulous and detailed.
-Yes, he asked for tip at the end but it is part of American culture, so be it. - KeyOne J 4 months ago  Flag as inappropriate Tommy is always very patient to my hair. He has been taking care of my hair for a few years and I am very happy with all the styles he has done for me. This is the most recent style. - Anna X 5 months ago  Flag as inappropriate Very accommodating and they fit you in the schedule quickly.  I appreciate that since I don’t always know my schedule too far in advance.  I think 15% tip is standard so I’m not sure why people are complaining.  Tommy spends a lot of time giving you a quality haircut. - Mary W Write a Review Read More Gallery Contact Us Contact Call now (814) 954-5812 Address Get directions 135 East Beaver Avenue State College, PA 16801 USA Business Hours 7 Mon: 10:00 AM – 8:00 PM Tue: 10:00 AM – 8:00 PM Wed: 10:00 AM – 8:00 PM Thu: 10:00 AM – 8:00 PM Fri: 10:00 AM – 8:00 PM Sat: 10:00 AM – 8:00 PM Sun: 10:00 AM – 8:00 PM Report abuse Header photo  230 Bayshore Blvd. San Francisco, CA 94124 by  Top salon Powered by  Google  Menu  Call  Directions Contact Us ✕ Message sent. We'll get back to you soon. This site uses cookies from Google. Learn more  552 Mass Ave #208, Second Floor, Cambridge, MA 02139. ''')
-# print(phone)
-print(address)
+    f.write(soup.prettify())
 
+social_media, contact_page = extract_links(links, url)
+phone = extract_phone(soup_text)
+# address = extract_address(soup.get_text())
+
+print(contact_page)
+print(social_media)
+print(phone)
+# print(address)
+# with open('soup.txt', 'w', encoding="utf-8") as f:
+#     for item in links:
+#         f.write(item+'\n')
 
 # for result in soup.findAll('div'):
 #     # title = result['title']
