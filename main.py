c70a9a7dbf1d8d09bc695acd0458a7ba86684be8
alexandrumosgoreanu <amosgoreanu@gmail.com>
2024-02-29 20:35:18 +0200

    Added multithreading

--- main.py
+++ main.py
@@ -1,46 +1,189 @@
+import time
 import requests
+from requests.adapters import HTTPAdapter
 from bs4 import BeautifulSoup
 from extraction_helpers import *
 from selenium_driver import *
 from csv_helpers import *
+import time
+from concurrent.futures import ThreadPoolExecutor, wait, as_completed
+from threading import Lock
+import multiprocessing
 
-
-domains_list = read_domains('./data/sample-websites.csv')       # read domains from csv
+cpu_count = multiprocessing.cpu_count()
+print(cpu_count)
 # print(domains_list[1])
 
-url = "http://" + domains_list[8]
-print(url)
-page = requests.get(url)                            # make request to url
-page_html = str(page.content) 
+# url = "http://" + domains_list[3]
+# print(url)
+# page = requests.get(url)                            # make request to url
+# page_html = str(page.content) 
 
-soup = BeautifulSoup(page.content, "lxml")          # parsing with lxml for speed vs. html.parser
+# soup = BeautifulSoup(page.content, "lxml")          # parsing with lxml for speed vs. html.parser
 
-body = soup.find('body').get_text()
-print(len(body))
-if(len(body) < 100):        # server side rendered website that needs to be accessed through selenium. TODO: find better metric to evaluate whether a site is client side rendered or not
-    print(True)
-    rendered_html = get_rendered_html(url)
-    soup = BeautifulSoup(rendered_html, "lxml")
+# body = soup.find('body').get_text()
+# print(len(body))
+# if(len(body) < 10000):        # server side rendered website that needs to be accessed through selenium. TODO: find better metric to evaluate whether a site is client side rendered or not
+    # print(True)
+social = 0
+ph = 0
+adr = 0
+down = 0
+scraped_data = []
+ph_lock = Lock()
+social_lock = Lock()
+down_lock = Lock()
+adr_lock = Lock()
+scraped_data_lock = Lock()
+total = 0
+total_lock = Lock()
 
-links = [link['href'] for link in soup.find_all('a', href = True)]           # getting all the links before extracting just the text from the bs object
+def scrape(domains):
+    global ph
+    global adr
+    global social
+    global down
+    global scraped_data
+    global total
+    driver = create_driver()
+    # print(domains, flush=True)
 
-soup_text = soup.get_text(" ", strip=True)           # converting to text in order to match phone and address
+    adapter = HTTPAdapter(max_retries=1)
+    session = requests.Session()
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    session.trust_env = False
+    
+    for domain in domains:
+        url = "http://" + domain
+        client_rendered = False
+        with total_lock:
+            print(f"{total} : {url}", flush=True)
+            total += 1
+
+        try:
+            page = session.get(url, timeout=10)                            # make request to url
+            # if page.status_code != 200:
+            #      raise Exception(f"Get failed for {url}")
+            
+            soup = BeautifulSoup(page.content, "lxml")          # parsing with lxml for speed vs. html.parser
+            body = soup.find('body').get_text()
+            # driver.get(url)
+            # rendered_html = driver.page_source
+            # soup = BeautifulSoup(rendered_html, "lxml")
+            if(len(body) < 100):        # server side rendered website that needs to be accessed through selenium. TODO: find better metric to evaluate whether a site is client side rendered or not
+                client_rendered = True
+                print(f"_________{url} is client side rendered_____________")
+                try:
+                    driver.get(url)
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    rendered_html = driver.page_source
+                    soup = BeautifulSoup(rendered_html, "lxml")
+                except Exception as e:
+                    print(e)
+                    continue
+        except Exception as e:
+            print(e)
+            with down_lock:
+                print("______________Warning: " + url + " is down_______________", flush=True)
+                down += 1
+            with scraped_data_lock:
+                website_data = {
+                    "domain": domain,
+                    "phone": "n/a",
+                    "address": "n/a",
+                    "social_media": "n/a"
+                }
+                scraped_data.append(website_data)
+            continue
 
 
-social_media, contact_page = extract_links(links, url)
-phone = extract_phone(soup_text)
-address = extract_address(soup.get_text())
+        links = [link['href'] for link in soup.find_all('a', href = True)]           # getting all the links before extracting just the text from the bs object
 
-print(contact_page)
-print(social_media)
-print(phone)
-print(address)
+        soup_text = soup.get_text(" ", strip=True)           # converting to text in order to match phone and address
+        soup_text = soup_text.replace("\n", " ").replace("\t", " ").replace("\t", "")
 
-with open('soup.txt', 'w', encoding="utf-8") as f:
-    f.write(soup_text)
+        social_media, contact_pages = extract_links(links, url)
+        phone = extract_phone(soup_text)
+        address = extract_address(soup_text)
 
-# with open('soup.txt', 'w', encoding="utf-8") as f:
-#     for item in links:
-#         f.write(item+'\n')
+        if len(phone) == 0 and len(address) == 0 and len(contact_pages) != 0:
+            print(f'{url} has 0 data on the landing page')
+            for page in contact_pages:
+                print(f'Trying {page}')
+                if client_rendered == False:
+                    try:
+                        page = session.get(page, timeout=10)                            # make request to url
+                        soup = BeautifulSoup(page.content, "lxml")
+                    except Exception as e:
+                        print(e)
+                        continue
+                else:
+                    try:
+                        driver.get(page)
+                        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                        rendered_html = driver.page_source
+                        soup = BeautifulSoup(rendered_html, "lxml")
+                    except Exception as e:
+                        print(e)
+                        continue
+                soup_text = soup.get_text(" ", strip=True)           # converting to text in order to match phone and address
+                soup_text = soup_text.replace("\n", " ").replace("\t", " ").replace("\t", "")
 
-driver.quit()
+                social_media, contact_pages = extract_links(links, url)
+                phone = extract_phone(soup_text)
+                address = extract_address(soup_text)
+
+        if len(phone):
+            with ph_lock:
+                ph += 1
+        if len(social_media):
+            with social_lock:
+                social += + 1
+        if len(address):
+            with adr_lock:
+                adr += 1
+        with scraped_data_lock:
+            website_data = {
+                "domain": domain,
+                "phone": phone or "",
+                "address": address or "",
+                "social_media": social_media or "",
+                "contact_page": contact_pages or ""
+            }
+            scraped_data.append(website_data)
+
+        print(url + "is done_______________________________________________________________", flush=True)
+    # with open('soup.txt', 'w', encoding="utf-8") as f:
+    #     f.write(soup_text)
+    driver.quit()
+
+
+def divide_chunks(l, n): 
+    # looping till length l 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n] 
+
+if __name__ == '__main__':
+    start = time.time()
+    domains_list = read_domains('./data/sample-websites.csv')       # read domains from csv
+    # domains_list = domains_list[501:]
+    # domains_list = ["melatee.com"]
+    chunks = list(divide_chunks(domains_list, 42))                 # dividing the domains into (somehwat) equal chunks to be passed to a thread
+
+    # Getting the available amount of physical threads on the cpu
+    cpu_cores = multiprocessing.cpu_count()
+    with ThreadPoolExecutor(max_workers=cpu_cores*2) as executor:     # starting a thread pool with 2xnumber of threads as there are physical ones
+        # executor.map(scrape, chunks, chunksize=2)
+        futures = [executor.submit(scrape, chunk) for chunk in chunks]
+        for future in futures:
+            future.result()         # waiting for all threads to finish
+
+    end = time.time()
+   
+
+    for data in scraped_data:
+        print(data)
+    print(ph, adr, social, down)
+
+    print(f"It took %d seconds", end - start)
--- selenium_driver.py
+++ selenium_driver.py
@@ -4,18 +4,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException
 
-options = webdriver.ChromeOptions() 
-# options.add_argument("--headless")                  # Set the chrome webdriver to run in headless mode for scalability
-options.add_experimental_option("detach", True)     # Keep browser open until .quit() is called
-options.page_load_strategy = "normal"               # Wait until the ready state is complete
-driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(10)
+def create_driver():    
+    options = webdriver.ChromeOptions() 
+    options.add_argument("--headless=new")                  # Set the chrome webdriver to run in headless mode for scalability
+    options.add_experimental_option("detach", True)     # Keep browser open until .quit() is called
+    options.add_argument("--ignore-certificate-errors") # Set up the WebDriver with options to disable images
+    # options.add_argument("--disable-proxy-certificate-handler")
+    # options.add_argument("--disable-extensions")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--disable-impl-side-painting")
+    # options.add_argument("--disable-setuid-sandbox")
+    # options.add_argument("--disable-seccomp-filter-sandbox")
+    # options.add_argument("--disable-breakpad")
+    # options.add_argument("--disable-client-side-phishing-detection")
+    # options.add_argument("--disable-cast")
+    # options.add_argument("--disable-cast-streaming-hw-encoding")
+    # options.add_argument("--disable-cloud-import")  
+    # options.add_argument("--disable-popup-blocking")
+    # options.add_argument("--ignore-certificate-errors")
+    # options.add_argument("--disable-session-crashed-bubble")
+    # options.add_argument("--disable-ipv6")
+    # options.add_argument("--allow-http-screen-capture")
+    # options.add_argument("--disable-notifications")
+    # options.add_argument("--timeout=5")
+    options.page_load_strategy = "normal"               # Wait until the ready state is complete
+    driver = webdriver.Chrome(options=options)
+    driver.set_page_load_timeout(6)
+    return driver
 
-def get_rendered_html(url):
-    driver.get(url)
-    rendered_html = driver.page_source
-    return rendered_html
 
-def close_browser():
-    driver.quit()
