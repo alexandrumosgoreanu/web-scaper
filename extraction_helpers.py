9bc451c30a944c6201e8a6fe1daa0512045f193d
alexandrumosgoreanu <amosgoreanu@gmail.com>
2024-02-29 14:11:20 +0200

    Improved address regex

--- extraction_helpers.py
+++ extraction_helpers.py
@@ -15,9 +15,9 @@ address_regex = re.compile(r'''(
                            [a-zA-Z\s.\-\,\#]+
                            (\b([A|a]venue|[A|a]ve|[C|c]ourt|[C|c]t|[S|s]treet|[S|s]t|[D|d]rive|[D|d]r|[L|l]ane|[L|l]n|[R|r]oad|[R|r]d|[B|b]lvd|[P|p]laza|[P|p]arkway|[P|p]kwy))+
                            [\s]?
-                           [a-zA-Z0-9.\-\,\#]+
-                           [a-zA-Z0-9\s.\-\,\#]*
-                           [0-9]{5})''', re.VERBOSE)
+                           [a-zA-Z.\-\,\#]+
+                           [a-zA-Z\s.\-\,\#]*
+                           [0-9]{5})''', re.VERBOSE) # ([0-9]+[\s]*[a-zA-Z\s.\-\,\#]+(\b([A|a]venue|[A|a]ve|[C|c]ourt|[C|c]t|[S|s]treet|[S|s]t|[D|d]rive|[D|d]r|[L|l]ane|[L|l]n|[R|r]oad|[R|r]d|[B|b]lvd|[P|p]laza|[P|p]arkway|[P|p]kwy))+[\s]?[a-zA-Z0-9.\-\,\#]+[a-zA-Z0-9\s.\-\,\#]*[0-9]{5})
 pobox_regex = re.compile(r'''(\b(PO|Post)+[a-zA-Z\s0-9.\-\,\#]*[0-9]{5})''', re.VERBOSE)
                          
 # email_regex = re.compile(r'''(
@@ -26,7 +26,7 @@ pobox_regex = re.compile(r'''(\b(PO|Post)+[a-zA-Z\s0-9.\-\,\#]*[0-9]{5})''', re.
 #                         [a-zA-Z0-9.-]+
 #                         (\.[a-zA-Z]{2,4}))''', re.VERBOSE)
 
-social_media_regex = re.compile(r'^(http[s]?)?(://)?(www.)?(facebook|instagram|twitter|tiktok|linkedin|youtube)+(.com[/in])+[/a-zA-Z0-9.\_\-\,\#]*$', re.VERBOSE)
+social_media_regex = re.compile(r'^(http[s]?)?(://)?(www.)?(facebook|instagram|twitter|tiktok|linkedin|youtube)+(.com[/in])+[/a-zA-Z0-9.\_\-\,\#]+$', re.VERBOSE)
 
 def extract_phone(soup):
     matches = set()
