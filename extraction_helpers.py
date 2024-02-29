f079dc429748c48793c5d9db4c754b61d6bc98c5
alexandrumosgoreanu <amosgoreanu@gmail.com>
2024-02-29 14:49:37 +0200

    Improved phone regex for country code and address regex

--- extraction_helpers.py
+++ extraction_helpers.py
@@ -1,22 +1,22 @@
 import re
 
 phone_regex = re.compile(r'''(
-                        (?:\+?(\d{1,3}))?
-                        (\d{3}|\(\d{3}\))+
-                        (\s|-|\.)+
-                        (\d{3})+
-                        (\s|-|\.)+
-                        (\d{4})+
-                        (\s*(ext|x|ext.)\s*(\d{2,5}))?)''', re.VERBOSE)
+                         \(?\+?(\d{1,3})?\)?(-|\.)?
+                         (\d{3}|\(\d{3}\))+
+                         (\s|-|\.)+
+                         (\d{3})+
+                         (\s|-|\.)+
+                         (\d{4})+
+                         (\s*(ext|x|ext.)\s*(\d{2,5}))?)''', re.VERBOSE)
 
 address_regex = re.compile(r'''(
                            [0-9]+
                            [\s]*
-                           [a-zA-Z\s.\-\,\#]+
+                           [a-zA-Z\s.\.\-\,\#]{0,30}
                            (\b([A|a]venue|[A|a]ve|[C|c]ourt|[C|c]t|[S|s]treet|[S|s]t|[D|d]rive|[D|d]r|[L|l]ane|[L|l]n|[R|r]oad|[R|r]d|[B|b]lvd|[P|p]laza|[P|p]arkway|[P|p]kwy))+
                            [\s]?
-                           [a-zA-Z.\-\,\#]+
-                           [a-zA-Z\s.\-\,\#]*
+                           [a-zA-Z0-9\s.\-\,\#]+
+                           [a-zA-Z0-9\s.\-\,\#]*
                            [0-9]{5})''', re.VERBOSE) # ([0-9]+[\s]*[a-zA-Z\s.\-\,\#]+(\b([A|a]venue|[A|a]ve|[C|c]ourt|[C|c]t|[S|s]treet|[S|s]t|[D|d]rive|[D|d]r|[L|l]ane|[L|l]n|[R|r]oad|[R|r]d|[B|b]lvd|[P|p]laza|[P|p]arkway|[P|p]kwy))+[\s]?[a-zA-Z0-9.\-\,\#]+[a-zA-Z0-9\s.\-\,\#]*[0-9]{5})
 pobox_regex = re.compile(r'''(\b(PO|Post)+[a-zA-Z\s0-9.\-\,\#]*[0-9]{5})''', re.VERBOSE)
                          
@@ -31,10 +31,11 @@ social_media_regex = re.compile(r'^(http[s]?)?(://)?(www.)?(facebook|instagram|t
 def extract_phone(soup):
     matches = set()
     for groups in phone_regex.findall(soup):
-        phone_numbers = '-'.join([groups[2], groups[4], groups[6]])
-        if groups[8] != '':
-                phone_numbers += ' x' + groups[8]            
-        matches.add(phone_numbers)
+        # prefix = groups[2].replace('(', '').replace(')', '')
+        # phone_numbers = '-'.join([prefix, groups[4], groups[6]])
+        # if groups[8] != '':
+                # phone_numbers += ' x' + groups[8]            
+        matches.add(groups[0])
 
     return matches
 
