6d6e06358e8f4411a812c2f57c05beaa60d4b486
alexandrumosgoreanu <amosgoreanu@gmail.com>
2024-03-03 17:57:23 +0200

    Moved ES query logic to es helpers

--- elasticsearch_helpers.py
+++ elasticsearch_helpers.py
@@ -1,16 +1,12 @@
 import json 
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
-
-INDEX_NAME = "website_data"
-ELASTIC_URL = "http://localhost:9200"
+from elasticsearch_dsl.query import FunctionScore
+INDEX_NAME = 'website_data'
+ELASTIC_URL = 'http://localhost:9200'
 
 f = open('data_best.json')
-scraped_results = json.load(f)
 
-# json_file = json.dumps(scraped_results)
-# with open("sample.json", "w") as outfile: 
-#     outfile.write(json_file)
 def connect_to_elastic():   
     try:
         es = Elasticsearch(ELASTIC_URL, verify_certs=False)
@@ -19,9 +15,29 @@ def connect_to_elastic():
     print('Connected to elastic')
     return es
 
-i=0
-def write_to_elastic(data):
-    for item in data:
-        index_result = es.index(index=INDEX_NAME, id=item['domain'], body=item)
-        print(f"{i} : {index_result}")
-        i+=1
+def query_company_data(es, item):
+    query_list = []
+    if item['input_name']:
+        query_list.append(Q('match', company_all_available_names = item['input_name']))
+
+        # Splitting the input name and search for wildcards inside the social media link and the domain url
+        input_name_strings = item['input_name'].split(' ')
+        for string in input_name_strings:
+            wildcard = '*' + ''.join(i for i in string.lower() if i.isalpha()) + '*'
+            if wildcard != '**':
+                wildcard_query = Q('wildcard', domain = wildcard)
+                function_score = FunctionScore(query = wildcard_query, boost = 5.0)     # boosting wildcard matches in domain
+                query_list.append(function_score)
+                query_list.append(Q('wildcard', social_media = wildcard))
+
+    if item['input_phone']:
+        query_list.append(Q('match', phone = item['input_phone']))
+    if item['input_website']:
+        query_list.append(Q('match', domain = item['input_website'].replace('https', '').replace('http', '').replace(':', '').replace('//', '').replace('www.', '').split('/', 1)[0]))  # extracting only domain url
+    if item['input_facebook']:
+        query_list.append(Q('match', social_media = item['input_facebook']))
+
+    query = Search(using=es, index='website_data').query(Q('bool', should = query_list,  minimum_should_match=1))
+    # query = query[:1]                                        
+    response = query.execute()
+    return response
